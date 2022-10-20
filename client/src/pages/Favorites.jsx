32af781bfe29372ac6f72c30590fae577ccefacd
charonchai <charonchai2541@gmail.com>
2022-10-20 18:27:40 +0700

    remove import not use

--- client/src/pages/Favorites.jsx
+++ client/src/pages/Favorites.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import "./Books.css";
 import StarIcon from '@mui/icons-material/Star';
 import { useNavigate } from "react-router-dom";
